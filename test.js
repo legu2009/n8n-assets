e8be9f9c358a037a4aaa387e9e5bc47ef0ad00cf
locker <locker.gu@gaiaworks.cn>
2025-06-19 17:45:36 +0800

    .

---
+++ test.js
@@ -0,0 +1,41 @@
+fetch("https://fjagxp-byxkfb-5678.app.cloudstudio.work/home/workflows", {
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7",
+      "cache-control": "max-age=0",
+      "priority": "u=0, i",
+      "sec-ch-ua": "\"Google Chrome\";v=\"137\", \"Chromium\";v=\"137\", \"Not/A)Brand\";v=\"24\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-origin",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1",
+      "cookie": "rl_page_init_referrer=RudderEncrypt%3AU2FsdGVkX1%2BhxY%2FpOmID1KdSvF3nfP%2FknrZArboP7BN6mmmGO7n5Vm0SDl1KqptWBcc9%2FZ2QltPwZ7mMtWpVbg%3D%3D; rl_page_init_referring_domain=RudderEncrypt%3AU2FsdGVkX1%2BnmZnfnaaQR01RSt%2FgcNR2NGCGrlJr2b8Fix3vAcP6t70UyGdBQPDh; n8n-auth=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM2MjE3OWFhLTlmMjAtNGNlZC04MjAwLTZkNTk4NWI0Njk5NyIsImhhc2giOiJzU0JlT1krYk8wIiwiYnJvd3NlcklkIjoiTzdDN0RPdWo1WkpqMS80Znp6aHRvTGxRNURERjZ4QVdOUitLTUE3bSs3QT0iLCJpYXQiOjE3NTAzMDM3ODEsImV4cCI6MTc1MDkwODU4MX0.srz7ETOeK3T72II-a6mC7eg7Hh5pE0zhnR31AMqvN4s; rl_anonymous_id=RudderEncrypt%3AU2FsdGVkX1%2FT7U%2FgNhxJ15ONQupAEfglZKaDwL0y7No8iRruXr9cJSY5uqUimgmQjfnW4ifcDHSLkAFkoeKI4g%3D%3D; rl_user_id=RudderEncrypt%3AU2FsdGVkX19oda7kR4eSj2UoB2uN3pOCPC6%2Bisp%2BeC1D0e7WC2JeTimb0eHIXBd0iALKOSqg9eDa%2FbpzVeroARdNEpqw7tKK7ZllZFlVgveXextGnWRQq7u7DfHz8%2BArunY1pwKAdTJYAwigrIwgYdsbqmuhy2O9oOQEVqUPuxE%3D; rl_trait=RudderEncrypt%3AU2FsdGVkX182NrKBv4idd3snPDsrcgMSIiVGdBvOBQWZ%2FiW9IvE8F2JVrrpXn4OGt5MIfTuPOFZzwpfFsyDftkJT8rUCFvyPLVekHndWtO1TaZwDb4GtH17eNggPgtMFbcy%2BDmitOhMw3P3kKPIdCA%3D%3D; rl_session=RudderEncrypt%3AU2FsdGVkX1%2BTKXahYmTqSoadk4okbIsNUovgUmI0uxbDh%2BjV%2B%2BMcpavCdXqOY7Bek0VSC6i7h8NIiFt%2BsbbMgbkau0BosCrejWWPnG%2BWoVkbhDZj%2BUwXwXuN2uXxy0Mo75Af9bflScGAcvwTIwiLvQ%3D%3D; ph_phc_4URIAm1uYfJO7j8kWSe0J8lc8IqnstRLS7Jx8NcakHo_posthog=%7B%22distinct_id%22%3A%2298580682ad2757720db08811f8afb16c16409afa905e0a3ff00818b4b6ca93cf%23362179aa-9f20-4ced-8200-6d5985b46997%22%2C%22%24sesid%22%3A%5B1750321637142%2C%220197874c-a71a-717d-899c-4d964c5bc7ab%22%2C1750321637142%5D%2C%22%24epp%22%3Atrue%2C%22%24initial_person_info%22%3A%7B%22r%22%3A%22%24direct%22%2C%22u%22%3A%22https%3A%2F%2Ffjagxp-byxkfb-5678.app.cloudstudio.work%2Fsetup%22%7D%7D"
+    },
+    "referrerPolicy": "no-referrer",
+    "body": null,
+    "method": "GET"
+  });
+
+  fetch("https://fjagxp-byxkfb-5679.app.cloudstudio.work/home/workflows", {
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7",
+      "cache-control": "max-age=0",
+      "priority": "u=0, i",
+      "sec-ch-ua": "\"Google Chrome\";v=\"137\", \"Chromium\";v=\"137\", \"Not/A)Brand\";v=\"24\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-origin",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1",
+      "cookie": "rl_page_init_referrer=RudderEncrypt%3AU2FsdGVkX1%2BhxY%2FpOmID1KdSvF3nfP%2FknrZArboP7BN6mmmGO7n5Vm0SDl1KqptWBcc9%2FZ2QltPwZ7mMtWpVbg%3D%3D; rl_page_init_referring_domain=RudderEncrypt%3AU2FsdGVkX1%2BnmZnfnaaQR01RSt%2FgcNR2NGCGrlJr2b8Fix3vAcP6t70UyGdBQPDh; n8n-auth=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM2MjE3OWFhLTlmMjAtNGNlZC04MjAwLTZkNTk4NWI0Njk5NyIsImhhc2giOiJzU0JlT1krYk8wIiwiYnJvd3NlcklkIjoiTzdDN0RPdWo1WkpqMS80Znp6aHRvTGxRNURERjZ4QVdOUitLTUE3bSs3QT0iLCJpYXQiOjE3NTAzMDM3ODEsImV4cCI6MTc1MDkwODU4MX0.srz7ETOeK3T72II-a6mC7eg7Hh5pE0zhnR31AMqvN4s; rl_anonymous_id=RudderEncrypt%3AU2FsdGVkX1%2FT7U%2FgNhxJ15ONQupAEfglZKaDwL0y7No8iRruXr9cJSY5uqUimgmQjfnW4ifcDHSLkAFkoeKI4g%3D%3D; rl_user_id=RudderEncrypt%3AU2FsdGVkX19oda7kR4eSj2UoB2uN3pOCPC6%2Bisp%2BeC1D0e7WC2JeTimb0eHIXBd0iALKOSqg9eDa%2FbpzVeroARdNEpqw7tKK7ZllZFlVgveXextGnWRQq7u7DfHz8%2BArunY1pwKAdTJYAwigrIwgYdsbqmuhy2O9oOQEVqUPuxE%3D; rl_trait=RudderEncrypt%3AU2FsdGVkX182NrKBv4idd3snPDsrcgMSIiVGdBvOBQWZ%2FiW9IvE8F2JVrrpXn4OGt5MIfTuPOFZzwpfFsyDftkJT8rUCFvyPLVekHndWtO1TaZwDb4GtH17eNggPgtMFbcy%2BDmitOhMw3P3kKPIdCA%3D%3D; rl_session=RudderEncrypt%3AU2FsdGVkX1%2BTKXahYmTqSoadk4okbIsNUovgUmI0uxbDh%2BjV%2B%2BMcpavCdXqOY7Bek0VSC6i7h8NIiFt%2BsbbMgbkau0BosCrejWWPnG%2BWoVkbhDZj%2BUwXwXuN2uXxy0Mo75Af9bflScGAcvwTIwiLvQ%3D%3D; ph_phc_4URIAm1uYfJO7j8kWSe0J8lc8IqnstRLS7Jx8NcakHo_posthog=%7B%22distinct_id%22%3A%2298580682ad2757720db08811f8afb16c16409afa905e0a3ff00818b4b6ca93cf%23362179aa-9f20-4ced-8200-6d5985b46997%22%2C%22%24sesid%22%3A%5B1750321637142%2C%220197874c-a71a-717d-899c-4d964c5bc7ab%22%2C1750321637142%5D%2C%22%24epp%22%3Atrue%2C%22%24initial_person_info%22%3A%7B%22r%22%3A%22%24direct%22%2C%22u%22%3A%22https%3A%2F%2Ffjagxp-byxkfb-5678.app.cloudstudio.work%2Fsetup%22%7D%7D"
+    },
+    "referrerPolicy": "no-referrer",
+    "body": null,
+    "method": "GET"
+  });
